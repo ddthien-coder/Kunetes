64cb1af0ad4b8afa96a886f6b75eb2a36c0abbac
ddthien-coder <jungjihoonkr97@gmail.com>
2021-12-16 15:55:54 +0700

    first commit

---
+++ src/main/java/com/kunetes/KunetesApplication.java
@@ -0,0 +1,13 @@
+package com.kunetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KunetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KunetesApplication.class, args);
+    }
+
+}
